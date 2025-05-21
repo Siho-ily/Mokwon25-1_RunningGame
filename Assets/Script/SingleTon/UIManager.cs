21fad83936ae27eeb5edb4588a514968d4647558
TreeBomb-01 <htb010630@gmail.com>
2025-05-21 17:56:42 +0900

    score시스템 재시작 버튼 구현

--- Assets/Script/SingleTon/UIManager.cs
+++ Assets/Script/SingleTon/UIManager.cs
@@ -16,6 +16,7 @@ public class UIManager : MonoBehaviour
 
     public void OnRetryButtonClicked()
     {
+        GameOverPanel.SetActive(false);
         GameManager.Instance.gameSpeed = 1f; // 혹시 게임 정지돼 있었다면 복구
         ScoreManager.Instance.ResetScore();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // 씬 재시작
